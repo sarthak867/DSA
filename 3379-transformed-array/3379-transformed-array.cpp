983956f48599912ef1158ddd9a89dff240d3cf39
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-02-05 09:43:21 +0530

    Time: 4 ms (79.67%), Space: 25.8 MB (8.08%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -4,8 +4,7 @@ public:
         vector<int> ans;
         int n = nums.size();
         for(int i =0 ; i <n; i++){
-            int ti = i + nums[i];
-            ti %= n;
+            int ti = (i + nums[i])%n;
             if(ti < 0){
                 ans.push_back(nums[n + ti]);
             }else ans.push_back(nums[ti]);
