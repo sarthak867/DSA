d6d230e73d4ce966bd737234de7a9cceb673ffd6
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-02-16 12:24:47 +0530

    Time: 398 ms (26.13%), Space: 369.6 MB (17.59%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    unordered_map<int, int> suspicious;
+
+    // Check if method k calls any suspicious method
+    bool check(unordered_map<int, vector<int>>& pc, int k) {
+        for (int i : pc[k]) {
+            if (suspicious.count(i))
+                return true;
+        }
+        return false;
+    }
+
+    // DFS to mark suspicious
+    void sus(unordered_map<int, vector<int>>& pc, int k) {
+        if (suspicious.count(k)) return;  // prevent infinite loop
+        
+        suspicious[k]++;
+
+        for (int i : pc[k]) {
+            sus(pc, i);
+        }
+    }
+
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& rel) {
+        
+        unordered_map<int, vector<int>> pc;
+
+        // build graph
+        for (auto &i : rel) {
+            pc[i[0]].push_back(i[1]);
+        }
+
+        // mark suspicious
+        sus(pc, k);
+
+        bool all = false;
+
+        // check safe methods
+        for (int i = 0; i < n; i++) {
+            if (!suspicious.count(i)) {
+                if (check(pc, i)) {
+                    all = true;
+                    break;
+                }
+            }
+        }
+
+        vector<int> ans;
+
+        if (all) {
+            for (int i = 0; i < n; i++)
+                ans.push_back(i);
+        } else {
+            for (int i = 0; i < n; i++) {
+                if (!suspicious.count(i))
+                    ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
