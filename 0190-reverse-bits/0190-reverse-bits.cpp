e1363da610f2641f1d3a7c2a970111be01ebfdb8
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-02-16 11:03:34 +0530

    Time: 0 ms (100%), Space: 8.6 MB (6.51%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        string bin="";
+        while(n>0){
+            int rem = (n%2);
+            n /= 2;
+            char c = rem + '0';
+            bin += c;
+        }
+        while(bin.length()!=32){
+            bin += '0';
+        }
+        int ans =0;
+        long long m =1;
+        for(int i = bin.length()-1; i>=0 ; i--){
+            int bit = bin[i] - '0';
+            int de = bit * m;
+            ans += de;
+            m *= 2;
+        }
+        return ans;
+    }
+};
