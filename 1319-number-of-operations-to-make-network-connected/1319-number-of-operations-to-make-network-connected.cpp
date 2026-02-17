299301b261a0454c486ed809935484b679341b4c
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-02-17 10:33:57 +0530

    Time: 58 ms (5.14%), Space: 60.5 MB (5.75%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    unordered_map<int, vector<int>> rel;
+    vector<int> vis;
+    
+    void check(int i){
+        if(vis[i] == 1) return;
+        
+        vis[i] = 1;
+        
+        for(int j : rel[i]){
+            check(j);
+        }
+    }
+
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        
+        if(connections.size() < n-1) return -1;
+
+        vis.assign(n, 0);
+
+        // build undirected graph
+        for(auto &v : connections){
+            rel[v[0]].push_back(v[1]);
+            rel[v[1]].push_back(v[0]);
+        }
+
+        int components = 0;
+
+        for(int i = 0; i < n; i++){
+            if(!vis[i]){
+                check(i);
+                components++;
+            }
+        }
+
+        return components - 1;
+    }
+};
