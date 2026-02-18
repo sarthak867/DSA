bcd054b1946f8860f6a761d0f808403f60dff765
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-02-18 11:10:50 +0530

    Time: 251 ms (17.35%), Space: 230.1 MB (24.49%) - LeetHub

---
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions,
+                                      vector<int>& healths, string directions) {
+
+        int n = positions.size();
+        map<int, pair<int, char>> robo;
+
+        for (int i = 0; i < n; i++) {
+            robo[positions[i]] = {healths[i], directions[i]};
+        }
+
+        vector<int> res;
+        stack<int> st; 
+
+        for (auto& it : robo) {
+
+            int pos = it.first;
+            int& hel = it.second.first;
+            char dir = it.second.second;
+
+            if (dir == 'R') {
+                st.push(pos);
+            } else { // dir == 'L'
+
+                while (!st.empty() && hel > 0) {
+
+                    int prevPos = st.top();
+                    int& prevHel = robo[prevPos].first;
+
+                    if (prevHel < hel) {
+                        st.pop();
+                        prevHel = 0;
+                        hel--;
+                    } else if (prevHel == hel) {
+                        st.pop();
+                        prevHel = 0;
+                        hel = 0;
+                    } else {
+                        prevHel--;
+                        hel = 0;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (robo[positions[i]].first > 0)
+                res.push_back(robo[positions[i]].first);
+        }
+
+        return res;
+    }
+};
