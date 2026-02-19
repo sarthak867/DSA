27e17c0955771797f5b6dd25f151aed2e7c9b9a1
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-02-19 10:50:25 +0530

    Time: 15 ms (5.39%), Space: 13.1 MB (61.91%) - LeetHub

---
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+
+        int n = s.length();
+        if(n < 2) return 0;
+
+        int ans = 0;
+
+        for(int i = 0; i < n - 1; i++) {
+
+            // group change detect karo
+            if(s[i] != s[i+1]) {
+
+                char leftChar = s[i];
+                char rightChar = s[i+1];
+
+                int l = i;
+                int r = i + 1;
+
+                while(l >= 0 && r < n &&
+                      s[l] == leftChar &&
+                      s[r] == rightChar) {
+
+                    ans++;
+                    l--;
+                    r++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
