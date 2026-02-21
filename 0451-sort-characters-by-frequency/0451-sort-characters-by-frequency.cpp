b39957adebed49600c4f504db0cedb3e34023b0f
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-02-21 14:48:20 +0530

    Time: 0 ms (100%), Space: 11.4 MB (30.92%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -4,24 +4,23 @@ public:
         unordered_map<char, int> freq;
         map<int, vector<char>> freq2;
 
-        for(char c : s){
+        for(char c : s) {
             freq[c]++;
         }
 
-        for(auto &mp : freq){
+        for(auto &mp : freq) {
             freq2[mp.second].push_back(mp.first);
         }
 
         string result = "";
 
-        for(auto it = freq2.rbegin(); it != freq2.rend(); it++){
-            int count = it->first;
-
-            for(char c : it->second){
-                result += string(count, c);
+        for(auto &mp : freq2) {
+            for(char c : mp.second) {
+                result += string(mp.first, c);
             }
         }
 
+        reverse(result.begin(), result.end());  
         return result;
     }
 };
