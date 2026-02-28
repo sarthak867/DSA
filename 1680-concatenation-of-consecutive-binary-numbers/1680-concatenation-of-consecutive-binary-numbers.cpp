4715c23121f84b4230b1eca5ec5e9583ca177c2a
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-02-28 10:20:09 +0530

    Time: 48 ms (53.02%), Space: 8 MB (69.8%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,16 @@
+#define mod 1000000007
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        long long ans = 0;
+        int bits = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0)
+                bits++;
+            ans = ((ans << bits) % mod + i) % mod;
+        }
+
+        return ans;
+    }
+};
