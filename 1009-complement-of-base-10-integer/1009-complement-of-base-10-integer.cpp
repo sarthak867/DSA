484368ca59d71db46c86b648933806ea87b435c3
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-03-11 14:14:02 +0530

    Time: 0 ms (100%), Space: 7.9 MB (26.22%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
-        vector<long long> nums;
-        for (int i = 0; i <= 30; i++) {
-            nums.push_back((1LL << i)-1);
-        }
         if(n==0) return 1;
-        for(int i : nums){
-            if(n<=i) return i-n;
+        for(int i = 1 ; i<= 30 ; i++){
+            int num = (1<<i)-1;
+            if(n<=num) return num-n;
         }
-        return -1;
+        return 0;
     }
 };
