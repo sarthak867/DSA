29101e2407316ccbb4faa612683dcc90fc8ec8ed
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-03-29 11:03:54 +0530

    Time: 3 ms (5.46%), Space: 9.5 MB (18.58%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
+
+        unordered_map<int, vector<char>> mp = {
+            {2, {'a', 'b', 'c'}}, {3, {'d', 'e', 'f'}},
+            {4, {'g', 'h', 'i'}}, {5, {'j', 'k', 'l'}},
+            {6, {'m', 'n', 'o'}}, {7, {'p', 'q', 'r', 's'}},
+            {8, {'t', 'u', 'v'}}, {9, {'w', 'x', 'y', 'z'}}
+        };
+
+        vector<string> comb;
+
+        for (char c : digits) {
+            int n = c - '0';
+            vector<string> dup;
+
+            if (comb.empty()) {
+                for (char r : mp[n]) {
+                    comb.push_back(string(1, r)); // fix
+                }
+            } else {
+                for (string s : comb) {
+                    for (char ch : mp[n]) {
+                        dup.push_back(s + ch);
+                    }
+                }
+                comb = dup; // overwrite
+            }
+        }
+
+        return comb;
+    }
+};
