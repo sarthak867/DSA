988b77b1e3b1473a9dbf650a933cf2317bc9f461
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-04-07 13:48:27 +0530

    Time: 7 ms (27.7%), Space: 24.1 MB (33.47%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; i++) {
+            unordered_set<int> row, col;
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '0';
+                    if (row.count(num))
+                        return 0;
+                    row.insert(num);
+                }
+                if (board[j][i] != '.') {
+                    int num = board[j][i] - '0';
+                    if (col.count(num))
+                        return 0;
+                    col.insert(num);
+                }
+            }
+        }
+        unordered_set<int> b1, b2, b3;
+        for (int i = 0; i < 9; i++) {
+            if (i % 3 == 0) {
+                b1.clear();
+                b2.clear();
+                b3.clear();
+            }
+
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '0';
+                    if (b1.count(num))
+                        return 0;
+                    b1.insert(num);
+                }
+                if (board[i][j + 3] != '.') {
+                    int num = board[i][j + 3] - '0';
+                    if (b2.count(num))
+                        return 0;
+                    b2.insert(num);
+                }
+                if (board[i][j + 6] != '.') {
+                    int num = board[i][j + 6] - '0';
+                    if (b3.count(num))
+                        return 0;
+                    b3.insert(num);
+                }
+            }
+        }
+        return 1;
+    }
+};
