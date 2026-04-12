3caeb245b712b5faa361161d5aa2def5750a72cb
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-04-12 12:14:10 +0530

    Time: 18 ms (53.41%), Space: 9.2 MB (85.54%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    int getDistance(int p, int q) {
-        int x1 = p / 6, y1 = p % 6;
-        int x2 = q / 6, y2 = q % 6;
-        return abs(x1 - x2) + abs(y1 - y2);
+    int cal(int a, int b) {
+        int ax = a / 6, ay = a % 6, bx = b / 6, by = b % 6;
+        return abs(a / 6 - b / 6) + abs(a % 6 - b % 6);
     }
 
-    int minimumDistance(string word) {
-        int n = word.size();
-        vector<vector<int>> dp(n, vector<int>(26, INT_MAX >> 1));
-        fill(dp[0].begin(), dp[0].end(), 0);
+    int minimumDistance(string s) {
+        int n = s.size(), dp[300][26][26];
 
-        for (int i = 1; i < n; ++i) {
-            int cur = word[i] - 'A';
-            int prev = word[i - 1] - 'A';
-            int d = getDistance(prev, cur);
-            for (int j = 0; j < 26; ++j) {
-                dp[i][j] = min(dp[i][j], dp[i - 1][j] + d);
-                if (prev == j) {
-                    for (int k = 0; k < 26; ++k) {
-                        int d0 = getDistance(k, cur);
-                        dp[i][j] = min(dp[i][j], dp[i - 1][k] + d0);
-                    }
+        for (int i = 0; i < n; i++) {
+            int t = s[i] - 'A';
+            for (int j = 0; j < 26; j++) {
+                for (int k = 0; k < 26; k++) {
+                    dp[i + 1][j][k] = 1e6;
+                }
+            }
+            for (int j = 0; j < 26; j++) {
+                for (int k = 0; k < 26; k++) {
+                    dp[i + 1][j][t] = min(dp[i + 1][j][t], dp[i][j][k] + cal(k, t));
+                    dp[i + 1][t][k] = min(dp[i + 1][t][k], dp[i][j][k] + cal(j, t));
                 }
             }
         }
 
-        int ans = *min_element(dp[n - 1].begin(), dp[n - 1].end());
+        int ans = 1e6;
+        for (int j = 0; j < 26; j++) {
+            for (int k = 0; k < 26; k++) {
+                ans = min(ans, dp[n][j][k]);
+            }
+        }
         return ans;
     }
 };
