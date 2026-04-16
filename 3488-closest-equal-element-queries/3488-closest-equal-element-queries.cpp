b1744ef4760f6bff3513b2b53a971f880abd503a
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-04-16 16:17:48 +0530

    Time: 222 ms (55.72%), Space: 213.8 MB (68.66%) - LeetHub

--- 3488-closest-equal-element-queries/3488-closest-equal-element-queries.cpp
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.cpp
@@ -4,26 +4,21 @@ public:
         unordered_map<int, vector<int>> mp;
         int n = nums.size();
 
-        // store indices
         for (int i = 0; i < n; i++) {
             mp[nums[i]].push_back(i);
         }
 
-        vector<int> ans;
-        ans.reserve(queries.size());
-
-        for (int q : queries) {
+        for (int i = 0; i < queries.size(); i++) {
+            int q = queries[i];          // save original index
             int val = nums[q];
             auto &vec = mp[val];
 
             if (vec.size() == 1) {
-                ans.push_back(-1);
+                queries[i] = -1;
                 continue;
             }
 
-            // binary search
             int pos = lower_bound(vec.begin(), vec.end(), q) - vec.begin();
-
             int m = vec.size();
 
             int left = vec[(pos - 1 + m) % m];
@@ -32,11 +27,9 @@ public:
             int d1 = abs(q - left);
             int d2 = abs(q - right);
 
-            int res = min({d1, d2, n - d1, n - d2});
-
-            ans.push_back(res);
+            queries[i] = min({d1, d2, n - d1, n - d2});
         }
 
-        return ans;
+        return queries;
     }
 };
