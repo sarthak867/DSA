d6aa8caf2b1f50f70ab7f13014587d8481c89eff
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-04-26 10:18:17 +0530

    Time: 4 ms (45.64%), Space: 136.8 MB (65.83%) - LeetHub

--- 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.cpp
+++ 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     long long minOperations(vector<int>& nums) {
-        long long a = 0;
-
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i - 1] > nums[i]) {
-                a += 1LL * nums[i - 1] - nums[i];
+        long long total = 0;
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] < nums[i - 1]) {
+                total += (long long)(nums[i - 1] - nums[i]);
             }
         }
-
-        return a;
+        return total;
     }
 };
