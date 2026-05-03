8994d5c0594d2f3771b9e1adee9247d190f97be7
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-05-03 09:34:36 +0530

    Time: 0 ms (100%), Space: 8.7 MB (15.19%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -3,7 +3,10 @@ public:
     bool rotateString(string s, string g) {
         if(s==g) return 1;
         for(int i =1; i<s.size(); i++){
-            if(s[i]==g[0] && (s.substr(i) + s.substr(0, i )) ==g) return 1;
+            if(s[i]==g[0]){
+                string nw = s.substr(i) + s.substr(0, i );
+                if(nw==g) return 1;
+            }
         }
         return 0;
     }
