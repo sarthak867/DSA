70c08d22693ae8d91ae8a448c856f7cff1c2f517
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-05-05 14:38:56 +0530

    Time: 0 ms (100%), Space: 16.4 MB (63.05%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || !head->next || k == 0)
+            return head;
+
+        // Step 1: Copy list values into vector
+        vector<int> num;
+        for (ListNode* temp = head; temp; temp = temp->next)
+        num.push_back(temp->val);
+
+        int n = num.size();
+        k %= n;
+        k = n-k;
+        if (k == 0)
+            return head;
+
+        reverse(num.begin(), num.begin() + k);
+        reverse(num.begin() + k, num.end());
+
+        // Copy values back to list
+        ListNode* temp = head;
+        int i = n-1;
+        while (temp) {
+            temp->val = num[i--];
+            temp = temp->next;
+        }
+
+        return head;
+    }
+};
