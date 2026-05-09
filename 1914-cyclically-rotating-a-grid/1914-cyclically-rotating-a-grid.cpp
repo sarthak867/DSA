6a635fd82ca7f3fb0f8f4eeda8cb722a10ff9487
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-05-09 19:22:48 +0530

    Time: 19 ms (22.06%), Space: 16.6 MB (94.85%) - LeetHub

--- 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.cpp
+++ 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.cpp
@@ -1,44 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
-        int m = grid.size();
-        int n = grid[0].size();
-        int nlayer = min(m / 2, n / 2);  // level count
-        // enumerate each layer counterclockwise starting from the top-left
-        // corner
-        for (int layer = 0; layer < nlayer; ++layer) {
-            vector<int> r, c,
-                val;  // each element's row index, column index, and value
-            for (int i = layer; i < m - layer - 1; ++i) {  // left
-                r.push_back(i);
-                c.push_back(layer);
-                val.push_back(grid[i][layer]);
+        int T = 0, L = 0;
+        int B = grid.size() - 1, R = grid[0].size() - 1;
+
+        while (T < B && L < R) {
+            int len = B - T, wid = R - L;
+            int perimeter = 2 * len + 2 * wid;
+            int r = k % perimeter;
+
+            while (r--) {
+                int tmp = grid[T][L];
+
+                for (int i = L; i < R; i++)
+                    grid[T][i] = grid[T][i + 1];
+
+                for (int i = T; i < B; i++)
+                    grid[i][R] = grid[i + 1][R];
+
+                for (int i = R; i > L; i--)
+                    grid[B][i] = grid[B][i - 1];
+
+                for (int i = B; i > T; i--)
+                    grid[i][L] = grid[i - 1][L];
+
+                grid[T + 1][L] = tmp;
             }
-            for (int j = layer; j < n - layer - 1; ++j) {  // down
-                r.push_back(m - layer - 1);
-                c.push_back(j);
-                val.push_back(grid[m - layer - 1][j]);
-            }
-            for (int i = m - layer - 1; i > layer; --i) {  // right
-                r.push_back(i);
-                c.push_back(n - layer - 1);
-                val.push_back(grid[i][n - layer - 1]);
-            }
-            for (int j = n - layer - 1; j > layer; --j) {  // up
-                r.push_back(layer);
-                c.push_back(j);
-                val.push_back(grid[layer][j]);
-            }
-            int total = val.size();  // total number of elements in each layer
-            int kk = k % total;      // equivalent number of rotations
-            // find the value at each index after rotation
-            for (int i = 0; i < total; ++i) {
-                int idx =
-                    (i + total - kk) % total;  // the index corresponding to the
-                                               // value after rotation
-                grid[r[i]][c[i]] = val[idx];
-            }
+
+            T++; L++;
+            B--; R--;
         }
+
         return grid;
     }
 };
