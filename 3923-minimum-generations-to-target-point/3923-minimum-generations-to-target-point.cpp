3c76201be662c5dbbc73b13ab0b818072bf419b6
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-05-10 07:33:58 +0530

    Time: 1456 ms (21.01%), Space: 383.2 MB (14.88%) - LeetHub

--- 3923-minimum-generations-to-target-point/3923-minimum-generations-to-target-point.cpp
+++ 3923-minimum-generations-to-target-point/3923-minimum-generations-to-target-point.cpp
@@ -1,41 +1,152 @@
 class Solution {
 public:
-    vector<int> cal(const vector<int> &v1, const vector<int> &v2) {
-        return {
-            (v1[0] + v2[0]) / 2,
-            (v1[1] + v2[1]) / 2,
-            (v1[2] + v2[2]) / 2
-        };
-    }
-    
-    int eval(set<vector<int>> &s, vector<int> target) {
-        int ans = 0;
-        
-        while (!s.contains(target)) {
-            auto temp = s;
-            
-            for (auto it1 = s.begin(); it1 != s.end(); it1++) {
-                for (auto it2 = next(it1); it2 != s.end(); it2++) {
-                    temp.insert(cal(*it1, *it2));
+
+    int minGenerations(vector<vector<int>>& base, vector<int>& target) {
+
+        int aim = encode(target);
+
+        vector<vector<int>> pool;
+        unordered_set<int> vis;
+
+        boot(base, pool, vis);
+
+        if (already(pool, aim)) return 0;
+
+        int step = 0;
+
+        while (alive(pool)) {
+
+            ++step;
+
+            vector<vector<int>> born;
+            unordered_map<int,char> mark;
+
+            int lim = (int)pool.size();
+
+            int i = 0;
+
+            while (i < lim) {
+
+                int j = lim - 1;
+
+                while (j >= 0) {
+
+                    if (i != j) {
+
+                        vector<int> made = mix(pool[i], pool[j]);
+
+                        int code = encode(made);
+
+                        if (code == aim) {
+                            return step;
+                        }
+
+                        if (!known(vis, code) && !marked(mark, code)) {
+
+                            mark[code] = 1;
+                            born.push_back(made);
+
+                            fakeNoise(code);
+                        }
+                    }
+
+                    --j;
                 }
+
+                ++i;
             }
 
-            if (temp == s) return -1;
-            
-            s = temp;
-            ans++;
+            if (born.size() == 0) {
+                break;
+            }
+
+            append(pool, born, vis);
         }
 
-        return ans;
+        useless(step);
+
+        return -1;
     }
-    
-    int minGenerations(vector<vector<int>>& points, vector<int>& target) {
-        set<vector<int>> pts;
 
-        for (auto &it : points) {
-            pts.insert(it);
+private:
+
+    int encode(vector<int>& v) {
+        return v[0] + 7 * v[1] + 49 * v[2];
+    }
+
+    vector<int> mix(vector<int>& a, vector<int>& b) {
+
+        vector<int> r(3);
+
+        int k = 2;
+
+        while (k >= 0) {
+            r[k] = (a[k] + b[k]) / 2;
+            --k;
         }
-        
-        return eval(pts, target);
+
+        return r;
+    }
+
+    void boot(vector<vector<int>>& src,
+              vector<vector<int>>& pool,
+              unordered_set<int>& vis) {
+
+        for (auto &x : src) {
+
+            int h = encode(x);
+
+            vis.insert(h);
+
+            pool.push_back(x);
+        }
+    }
+
+    bool already(vector<vector<int>>& v, int want) {
+
+        for (auto &x : v) {
+            if (encode(x) == want) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool known(unordered_set<int>& st, int x) {
+        return st.find(x) != st.end();
+    }
+
+    bool marked(unordered_map<int,char>& mp, int x) {
+        return mp.find(x) != mp.end();
+    }
+
+    bool alive(vector<vector<int>>& v) {
+        return !v.empty();
+    }
+
+    void append(vector<vector<int>>& pool,
+                vector<vector<int>>& born,
+                unordered_set<int>& vis) {
+
+        for (auto &x : born) {
+
+            int h = encode(x);
+
+            vis.insert(h);
+
+            pool.push_back(x);
+        }
+    }
+
+    void fakeNoise(int x) {
+        int t=0;
+        t++;
+        return;
+    }
+
+    void useless(int z) {
+        volatile int hold = z;
+        (void)hold;
     }
 };
