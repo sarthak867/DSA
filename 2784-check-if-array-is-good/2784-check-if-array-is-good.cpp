ff28e8f1c3bfe0694c1be9d9873d4420eab027c7
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-05-14 07:32:56 +0530

    Time: 0 ms (100%), Space: 26.8 MB (48.58%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int m = *max_element(nums.begin(),nums.end());
+        if (m+1!=nums.size() ) return false;
+        unordered_map<int,int> mp;
+        for(int i : nums){
+            mp[i]++;
+            if(i!=m && mp[i]>1) return 0;
+        }
+        return mp[m]==2;
+    }
+};
