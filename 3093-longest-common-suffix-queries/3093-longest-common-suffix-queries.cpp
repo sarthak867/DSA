9b9f0585c7cece98d2818168e5424fc68680effd
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-05-28 10:00:09 +0530

    Time: 61 ms (97.84%), Space: 153.6 MB (91.98%) - LeetHub

--- 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.cpp
+++ 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.cpp
@@ -1,85 +1,80 @@
-class Solution {
+class Trie {
 public:
-
-    struct Node {
-        int child[26];
-        int idx;
-
-        Node() {
-            memset(child, -1, sizeof(child));
-            idx = -1;
-        }
-    };
-
-    vector<Node> trie;
-
-    // update best index according to:
-    // 1. smaller length
-    // 2. earlier index
-    void updateIndex(int &storedIdx,int newIdx,vector<string>& wordsContainer) {
-        if(storedIdx == -1) {
-            storedIdx = newIdx;
-            return;
-        }
-
-        int oldLen =wordsContainer[storedIdx].size();
-        int newLen =wordsContainer[newIdx].size();
-
-        if(newLen < oldLen) {
-            storedIdx = newIdx;
-        }
-        else if(newLen == oldLen && newIdx < storedIdx) {
-            storedIdx = newIdx;
+    Trie() : tot_(0) {
+        for (int i = 0; i < N; i++) {
+            memset(tr_[i], 0, sizeof(tr_[i]));
+            min_len_[i] = INT_MAX;
+            idx_[i] = INT_MAX;
         }
     }
-
-    vector<int> stringIndices(vector<string>& wordsContainer,vector<string>& wordsQuery) {
-        trie.push_back(Node());
-
-        // build reverse trie
-        for(int i = 0;i < wordsContainer.size();i++) {
-            string word = wordsContainer[i];
-            reverse(word.begin(),word.end());
-
-            int node = 0;
-
-            // update root answer also
-            updateIndex(trie[node].idx,i,wordsContainer);
-
-            for(char ch : word) {
-                int c = ch - 'a';
-
-                if(trie[node].child[c] == -1) {
-                    trie[node].child[c] = trie.size(); //index of the new node
-                    //child['a'] = 5, means go to trie[5]
-                    trie.push_back(Node());
-                }
-
-                node = trie[node].child[c];
-                updateIndex(trie[node].idx,i,wordsContainer);
+    void clear() {
+        for (int i = 0; i <= tot_; i++) {
+            memset(tr_[i], 0, sizeof(tr_[i]));
+            min_len_[i] = INT_MAX;
+            idx_[i] = INT_MAX;
+        }
+        tot_ = 0;
+    }
+    void insert(const string& s, int idx) {
+        int p = 0;
+        if (min_len_[p] > s.length()) {
+            min_len_[p] = s.length();
+            idx_[p] = idx;
+        }
+        for (auto& ch : s) {
+            int c = ch - 'a';
+            if (tr_[p][c] == 0) {
+                tr_[p][c] = ++tot_;
+            }
+            p = tr_[p][c];
+            if (min_len_[p] > s.length()) {
+                min_len_[p] = s.length();
+                idx_[p] = idx;
             }
         }
-
-        vector<int> ans;
-
-        // process queries
-        for(string query : wordsQuery) {
-            reverse(query.begin(),query.end());
-
-            int node = 0;
-
-            for(char ch : query) {
-                int c = ch - 'a';
-
-                if(trie[node].child[c] == -1) {
-                    break;
-                }
-                node = trie[node].child[c];
+    }
+    int query(const string& s) {
+        int p = 0;
+        for (auto& ch : s) {
+            int c = ch - 'a';
+            if (tr_[p][c] != 0) {
+                p = tr_[p][c];
+            } else {
+                break;
             }
+        }
+        return idx_[p];
+    }
 
-            ans.push_back(trie[node].idx);
+private:
+    static constexpr int N = 500010, M = 26;
+    int tot_;
+    int tr_[N][M];
+    int min_len_[N];
+    int idx_[N];
+};
+
+Trie tr;
+
+class Solution {
+public:
+    vector<int> stringIndices(vector<string>& wordsContainer,
+                              vector<string>& wordsQuery) {
+        tr.clear();
+        int n = wordsContainer.size();
+        int m = wordsQuery.size();
+        for (int i = 0; i < n; i++) {
+            string s = wordsContainer[i];
+            reverse(s.begin(), s.end());
+            tr.insert(s, i);
         }
 
-        return ans;
+        vector<int> res(m);
+        for (int i = 0; i < m; i++) {
+            string s = wordsQuery[i];
+            reverse(s.begin(), s.end());
+            res[i] = tr.query(s);
+        }
+        return res;
     }
 };
