44f8e21f8f4fec762bf0f3a19f0b5c42402575a7
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-06-01 06:28:07 +0530

    Time: 0 ms (100%), Space: 14.1 MB (77.9%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.begin(),cost.end());
+        int c=0,t=0;
+        for(int i = cost.size()-1; i>=0; i--){
+            c++;
+            if(c%3==0) continue;
+            t+=cost[i];
+        }
+        return t;
+    }
+};
