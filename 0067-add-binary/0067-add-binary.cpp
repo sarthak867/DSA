8eb4330caf300efbfdd77f987e81fa70c0962d53
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-06-03 19:58:33 +0530

    Time: 0 ms (100%), Space: 9 MB (35.77%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,33 +1,60 @@
-const auto __ = []()
-{
-    struct Leetcode
-    {
-        static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; }
-    };
-    std::atexit(&Leetcode::_);
-    return 0;
-}();
-
 class Solution {
 public:
+
     string addBinary(string a, string b) {
-        string res;
-        int carry = 0;
-        int i = a.length() - 1;
-        int j = b.length() - 1;
-        while (i >= 0 || j >= 0 || carry) {
-            if (i >= 0)
-                carry += a[i--] - '0';
-            if (j >= 0)
-                carry += b[j--] - '0';
-
-            res += carry % 2 + '0';
-            carry /= 2;
+        int s1 = a.size()-1;
+        int s2 = b.size()-1;
+        string ans ="";
+        int c=0;
+        while(s1>=0 && s2>=0){
+            if(a[s1]=='1'&& b[s2]=='1'){
+                if(c){
+                    ans+='1';
+                }else{
+                    ans+='0';
+                }
+                c=1;
+            }else if(a[s1]=='0' && b[s2]=='0'){
+                if(c){
+                    ans += '1';
+                    c=0;
+                }else ans +='0';
+            }
+            else{
+                if(c){
+                    ans +='0';
+                }else ans +='1';
+            }
+            s1--;
+            s2--;
         }
+        while(s1>=0){
+            if(c){
+                if(a[s1]=='1'){
+                    ans += '0';
+                }else{
+                    ans+= '1';
+                    c=0;
+                }
+            }else ans+= a[s1];
+            s1--;
 
-        reverse(begin(res), end(res));
-        return res;
+        }
+        while(s2>=0){
+            if(c){
+                if(b[s2]=='1'){
+                    ans += '0';
+                }else{
+                    ans+= '1';
+                    c=0;
+                }
+            }else ans+= b[s2];
+            s2--;
+        }
+        if(c){
+            ans += '1';
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
-
-}
-;
+};
