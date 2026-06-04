1e3b778bd02472d401ee6252f42eb70ecc8b01ab
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-06-04 08:28:19 +0530

    Time: 47 ms (61.94%), Space: 10.2 MB (33.96%) - LeetHub

---
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    int pv(int i){
+        string s = to_string(i);
+        int c=0;
+        for(int i = 1; i<s.size()-1; i++){
+            if( (s[i]>s[i+1] && s[i]>s[i-1]) || (s[i]<s[i+1] && s[i]<s[i-1]) )c++;
+        }
+        return c;
+    }
+
+    int totalWaviness(int num1, int num2) {
+        if(num2<=100) return 0;
+        int c=0;
+        while(num1<=num2){
+            c+=pv(num1);
+            num1++;
+        }
+        return c;
+    }
+};
