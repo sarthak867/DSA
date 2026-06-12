fb0c37a130b98b84d94e931ef9b7d25ec7e655c2
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-06-12 08:00:08 +0530

    Time: 447 ms (34.45%), Space: 395.8 MB (25.56%) - LeetHub

--- 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.cpp
+++ 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.cpp
@@ -1,80 +1,104 @@
 class Solution {
 public:
-    vector<int>per,depth;
-    vector<vector<int>>up;
-    int LOG=20;
-    const int MOD=1e9+7;
-    int power(long long a,long long b){
-        long long ans=1;
-        a%=MOD;
-        while(b>0){
-            if(b&1)ans=(ans*a)%MOD;
-            a=(a*a)%MOD;
-            b>>=1;
+    static constexpr long long MOD = 1e9 + 7;
+
+    vector<vector<int>> up;
+    vector<int> depth;
+    vector<long long> pow2;
+    int LOG;
+
+    void dfs(int u, int parent,
+             vector<vector<int>>& graph) {
+
+        up[u][0] = parent;
+
+        for (int j = 1; j < LOG; j++) {
+            up[u][j] = up[up[u][j - 1]][j - 1];
         }
-        return (int)ans;
+
+        for (int v : graph[u]) {
+            if (v == parent) continue;
+
+            depth[v] = depth[u] + 1;
+            dfs(v, u, graph);
+        }
     }
 
-    int countWays(int d){
-        if(d==0)return 0;
-        return power(2,d-1);
-    }
+    int lca(int a, int b) {
 
-    void dfs(int node,int p,vector<vector<int>>&adj){
-        per[node]=p;
-        up[node][0]=p;
-        for(int j=1;j<LOG;j++){
-            if(up[node][j-1]!=-1)up[node][j]=up[up[node][j-1]][j-1];
+        if (depth[a] < depth[b])
+            swap(a, b);
+
+        int diff = depth[a] - depth[b];
+
+        for (int j = LOG - 1; j >= 0; j--) {
+            if (diff & (1 << j))
+                a = up[a][j];
         }
-        for(auto&it:adj[node]){
-            if(it!=p){
-                depth[it]=depth[node]+1;
-                dfs(it,node,adj);
+
+        if (a == b)
+            return a;
+
+        for (int j = LOG - 1; j >= 0; j--) {
+            if (up[a][j] != up[b][j]) {
+                a = up[a][j];
+                b = up[b][j];
             }
         }
+
+        return up[a][0];
     }
 
-    int lca(int u,int v){
-        if(depth[u]<depth[v])swap(u,v);
-        int diff=depth[u]-depth[v];
-        for(int j=0;j<LOG;j++){
-            if(diff&(1<<j))
-                u=up[u][j];
+    vector<int> assignEdgeWeights(vector<vector<int>>& edges,
+                                  vector<vector<int>>& queries) {
+
+        int n = edges.size() + 1;
+
+        LOG = 20;
+
+        vector<vector<int>> graph(n + 1);
+
+        for (auto& e : edges) {
+            int u = e[0];
+            int v = e[1];
+
+            graph[u].push_back(v);
+            graph[v].push_back(u);
         }
-        if(u==v)
-            return u;
-        for(int j=LOG-1;j>=0;j--){
-            if(up[u][j]!=up[v][j]){
-                u=up[u][j];
-                v=up[v][j];
+
+        up.assign(n + 1, vector<int>(LOG));
+        depth.assign(n + 1, 0);
+
+        dfs(1, 1, graph);
+
+        pow2.assign(n + 1, 1);
+
+        for (int i = 1; i <= n; i++) {
+            pow2[i] = (pow2[i - 1] * 2) % MOD;
+        }
+
+        vector<int> ans;
+
+        for (auto& q : queries) {
+
+            int u = q[0];
+            int v = q[1];
+
+            if (u == v) {
+                ans.push_back(0);
+                continue;
             }
-        }
-        return up[u][0];
-    }
 
-    int dist(int u,int v){
-        int L=lca(u,v);
-        int d=depth[u]+depth[v]-2*depth[L];
-        return countWays(d);
-    }
+            int ancestor = lca(u, v);
 
-    vector<int> assignEdgeWeights(vector<vector<int>>& edges, vector<vector<int>>& queries) {
-        int n=edges.size()+2;
-        vector<vector<int>>adj(n);
-        for(auto&edge:edges){
-            int u=edge[0],v=edge[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            int dist =
+                depth[u] +
+                depth[v] -
+                2 * depth[ancestor];
+
+            ans.push_back(pow2[dist - 1]);
         }
-        per.resize(n,-1);
-        depth.resize(n,0);
-        up.resize(n,vector<int>(LOG,-1));
-        dfs(1,-1,adj);
-        vector<int>ans;
-        for(auto&q:queries){
-            int u=q[0],v=q[1];
-            ans.push_back(dist(u,v));
-        }
+
         return ans;
     }
 };
