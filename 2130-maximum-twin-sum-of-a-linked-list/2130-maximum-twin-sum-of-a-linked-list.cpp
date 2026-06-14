3315523560c512b8fd46cbfdd8e04e2caa5f192e
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-06-14 09:39:24 +0530

    Time: 0 ms (100%), Space: 120.4 MB (98.87%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,39 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        //only two nodes
-        if(head->next->next==nullptr){
-            return head->val+head->next->val;
+        ListNode *slow = head, *fast = head, *prev = nullptr;
+
+        while (fast && fast->next) {
+            fast = fast->next->next;
+
+            ListNode* nxt = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nxt;
         }
 
-        int result=0;
-        ListNode* p=head;
-        ListNode* q=nullptr;
-        traverse(head,p,result,q);
-        return result;
-    }
-   
-    void traverse(ListNode* head,ListNode* p,int &result,ListNode* &q)
-    {
-        if(p!=nullptr)
-        {
-            q=head->next;
-            traverse(head->next,p->next->next,result,q);
-            if(head->val + q->val > result)
-            {
-                result=head->val+q->val;
-            }
-            q=q->next;
+        int ans = 0;
+        ListNode *left = prev, *right = slow;
+
+        while (left) {
+            ans = max(ans, left->val + right->val);
+            left = left->next;
+            right = right->next;
         }
+
+        return ans;
     }
 };
