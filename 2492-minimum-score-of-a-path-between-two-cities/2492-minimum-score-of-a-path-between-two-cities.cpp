7cfdc119ca18da76609a15482fc50b011d995237
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-07-04 14:21:53 +0530

    Time: 8 ms (98.01%), Space: 107.8 MB (93.09%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,41 +1,43 @@
 constexpr int N=1e5+1;
-// union find class with rank
-int Root[N], Rank[N];
-class UnionFind {
-public:
-    UnionFind(int n){
-        iota(Root, Root+n, 0);
-        memset(Rank, 0, n*sizeof(int));
-    }
-
-    int Find(int x) {// compressed path
-        return (x == Root[x])? x :Root[x]=Find(Root[x]);
-    }
-
-    bool Union(int x, int y) {
-        x=Find(x), y=Find(y);
-        if (x==y) return 0;
-        if (Rank[x] > Rank[y]) swap(x, y);
-        Root[x] = y;
-        if (Rank[x] == Rank[y])
-            Rank[y]++;
-        return 1;
-    }
-    bool connected(int x, int y) { return Find(x) == Find(y); }
-};
+struct Edge{int v, w, nxt=-1;};
+Edge E[N*2];// undirected
+int eIdx=0;
+int adj[N];
+inline void addEdge(int u, int v, int w){
+    E[eIdx]={v, w, adj[u]};
+    adj[u]=eIdx++;
+}
+bitset<N> vis;
+int q[N], front, back;
 class Solution {
 public:
+    static void adjacent_ini(int n, vector<vector<int>>& roads){
+        eIdx=0;
+        memset(adj+1, -1, sizeof(int)*n);
+        for (auto& path : roads) {
+            const int u=path[0] , v=path[1], w=path[2];
+            addEdge(u, v, w);
+            addEdge(v, u, w);
+        }
+    }
+
     static int minScore(int n, vector<vector<int>>& roads)
     {      
-        UnionFind G(n+1); // 1-indexed
-        for (auto& path : roads) {
-            const int a=path[0] , b=path[1], d=path[2];
-            G.Union(a, b);
-        }
+        adjacent_ini(n, roads);
+        vis.reset();
         int dist=INT_MAX;
-        for(auto& path : roads){
-            const int a=path[0] , b=path[1], d=path[2];
-            if (G.connected(1, a)) dist=min(dist, d);
+        front=back=0;
+        q[back++]=1;// start from 1
+        vis[1]=1;
+        while (front<back){
+            int u=q[front++];
+            for(int idx=adj[u]; idx!=-1; idx=E[idx].nxt){
+                const int v=E[idx].v, w=E[idx].w;
+                dist=min(dist, w);
+                if (vis[v]) continue;
+                vis[v]=1;
+                q[back++]=v;
+            }
         }
         return dist;
     }
