2b0bf88f9cd3e327b651bb42544a85b94d0259a8
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-07-05 23:08:50 +0530

    Time: 6 ms (82.23%), Space: 10.3 MB (85.71%) - LeetHub

--- 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
@@ -1,43 +1,62 @@
-using PII = pair<int, int>;
-
+// reduced space
+constexpr int mod=1e9+7, N=100;
+using ll=long long;
+using int2=pair<int, int>;// (max sum, ways getting max sum)
+constexpr int2 None={0, -1};
+int2 dp[2][N];
 class Solution {
-private:
-    static constexpr int mod = (int)1e9 + 7;
-
 public:
-    void update(vector<vector<PII>>& dp, int n, int x, int y, int u, int v) {
-        if (u >= n || v >= n || dp[u][v].first == -1) {
-            return;
+    static vector<int> pathsWithMaxScore(vector<string>& board) {
+        const int n=board.size();
+        bool last=(n-1)&1;
+        fill_n(dp[last], n, None);// fill with {0, -1}
+        dp[last][n-1]={0, 1};
+        // last row
+        for(int j=n-2; j>=0; j--){
+            const char c=board[n-1][j];
+            if (c=='X') break;
+            dp[last][j].first=dp[last][j+1].first+c-'0';
+            dp[last][j].second=1;
         }
-        if (dp[u][v].first > dp[x][y].first) {
-            dp[x][y] = dp[u][v];
-        } else if (dp[u][v].first == dp[x][y].first) {
-            dp[x][y].second += dp[u][v].second;
-            if (dp[x][y].second >= mod) {
-                dp[x][y].second -= mod;
-            }
-        }
-    }
-
-    vector<int> pathsWithMaxScore(vector<string>& board) {
-        int n = board.size();
-        vector<vector<PII>> dp(n, vector<PII>(n, {-1, 0}));
-        dp[n - 1][n - 1] = {0, 1};
-        for (int i = n - 1; i >= 0; --i) {
-            for (int j = n - 1; j >= 0; --j) {
-                if (!(i == n - 1 && j == n - 1) && board[i][j] != 'X') {
-                    update(dp, n, i, j, i + 1, j);
-                    update(dp, n, i, j, i, j + 1);
-                    update(dp, n, i, j, i + 1, j + 1);
-                    if (dp[i][j].first != -1) {
-                        dp[i][j].first +=
-                            (board[i][j] == 'E' ? 0 : board[i][j] - '0');
-                    }
+        board[0][0]='0';
+        for(int i=n-2; i>=0; i--){
+            const char cR=board[i][n-1];
+            bool cur=i&1, prv=(i+1)&1;
+            if (cR=='X' || dp[prv][n-1].second==-1) 
+                dp[cur][n-1]=None;
+            else 
+                dp[cur][n-1]={dp[prv][n-1].first+cR-'0', 1};
+            for(int j=n-2; j>=0; j--){
+                const int c=board[i][j];
+                if (c=='X'){
+                    dp[cur][j]=None;
+                    continue;
                 }
+                auto& [r0, r1]=dp[cur][j+1];
+                auto& [d0, d1]=dp[prv][j];
+                auto& [s0, s1]=dp[prv][j+1];
+                int prvM=-1;
+                if (r1>0) prvM=max(prvM, r0);
+                if (d1>0) prvM=max(prvM, d0);
+                if (s1>0) prvM=max(prvM, s0);
+                if (prvM==-1) {
+                    dp[cur][j]=None; 
+                    continue;
+                }
+                ll ways=0;
+                ways+=(r1>0 && prvM==r0)?r1:0;
+                ways+=(d1>0 && prvM==d0)?d1:0;
+                ways+=(s1>0 && prvM==s0)?s1:0;
+                dp[cur][j]={prvM+c-'0',  ways%mod};
             }
         }
-        return dp[0][0].first == -1
-                   ? vector<int>{0, 0}
-                   : vector<int>{dp[0][0].first, dp[0][0].second};
+        auto& [sum, ways]=dp[0][0];
+        return (ways<=0)?vector<int>({0, 0}):vector<int>({sum, ways});
     }
 };
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
