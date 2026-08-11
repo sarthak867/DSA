3f74295e2c34d8df9189854474698f926cacc807
Sarthak Rajvanshi <sarthakrajvanshi124@gmail.com>
2026-08-11 14:58:14 +0530

    Time: 2 ms (73.09%), Space: 10.1 MB (78.95%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,45 +1,38 @@
 class Solution {
 public:
-    bool isSafe(vector <string>& board, int row, int col) {
-        // col
-        for (int i = 0; i < row; i++) {
-            if (board[i][col] == 'Q') return false;
-        }
 
-        // main diag
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') return false;
+    bool valid(vector<string>&board, int row, int col , int n){
+        for(int i =0 ; i<row; i++){
+            if(board[i][col]=='Q') return 0;
         }
-
-        // anti diag
-        for (int i = row - 1, j = col + 1; i >= 0 && j < board.size(); i--, j++) {
-            if (board[i][j] == 'Q') return false;
+        for(int i = row -1,j=col-1; i>=0 && j>=0; i--, j--){
+            if(board[i][j]=='Q') return 0;
         }
-
-        return true;
+        for(int i=row-1, j=col+1; i>=0 && j<n; i--, j++){
+            if(board[i][j]=='Q') return 0;
+        }
+        return 1;
     }
 
-    void solve(int n, vector <string>& board, int row, vector <vector <string>>& ans) {
-        if (row == n){
+    void sol(int row,int n, vector<string>&board, vector<vector<string>>&ans){
+        if(row == n) {
             ans.push_back(board);
             return;
         }
-
-        for (int col = 0; col < n; col++) {
-            if (isSafe(board, row, col)) {
-                board[row][col] = 'Q';
-                solve(n, board, row + 1, ans);
-                board[row][col] = '.'; // beautiful
+        for(int col = 0; col < n; col++){
+            if(valid(board, row, col, n)){
+                board[row][col]='Q';
+                sol(row+1,n,board,ans);
+                board[row][col]='.';
             }
         }
+
     }
 
     vector<vector<string>> solveNQueens(int n) {
-        vector <string> board(n, string(n, '.'));
-        vector <vector <string>> ans;
-
-        solve(n, board, 0, ans);
-
+        vector<vector<string>> ans;
+        vector<string> board(n,string(n,'.'));
+        sol(0,n,board,ans);
         return ans;
     }
 };
